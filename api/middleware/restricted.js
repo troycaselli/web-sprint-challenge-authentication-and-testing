8af08c35d98682c2d01b611c7b955a672c2c1c99
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-12-14 19:49:23 -0800

    created restricted middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,21 @@
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../secrets')
+
 module.exports = (req, res, next) => {
-  next();
+  
+  const token = req.headers.authorization
+  if(token) {
+    jwt.verify(token, JWT_SECRET, (error, decoded) => {
+      if(error) {
+        next({status: 401, message: 'token invalid'})
+      } else {
+        console.log(decoded)
+        next()
+      }
+    })
+  } else {
+    next({status: 401, message: 'token required'})
+  }
   /*
     IMPLEMENT
 
